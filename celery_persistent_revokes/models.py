babd4886825dc94ea21954ddbeeb1821a5e5abbd
Hugo Bessa <hugo@vinta.com.br>
2018-08-03 17:07:47 -0300

    fixes some lints

--- celery_persistent_revokes/models.py
+++ celery_persistent_revokes/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 
+
 class IndexedTimeStampedModel(models.Model):
     created = AutoCreatedField(_('created'), db_index=True)
     modified = AutoLastModifiedField(_('modified'), db_index=True)
 
     class Meta:
         abstract = True
+
 
 class CeleryTaskRevoke(IndexedTimeStampedModel):
     id = models.CharField(max_length=255, primary_key=True)
