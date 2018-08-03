bdb1277db9c072b27d74b26f8015cb6782eddf82
Hugo Bessa <hugo@vinta.com.br>
2018-08-03 16:27:16 -0300

    makes revokes default django model to be timestamped

--- celery_persistent_revokes/models.py
+++ celery_persistent_revokes/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 
+class IndexedTimeStampedModel(models.Model):
+    created = AutoCreatedField(_('created'), db_index=True)
+    modified = AutoLastModifiedField(_('modified'), db_index=True)
 
-class CeleryTaskRevoke(models.Model):
+    class Meta:
+        abstract = True
+
+class CeleryTaskRevoke(IndexedTimeStampedModel):
     id = models.CharField(max_length=255, primary_key=True)
